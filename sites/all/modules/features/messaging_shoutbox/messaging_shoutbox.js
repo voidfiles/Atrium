f8155a04e035303dca50f441a80b20826f3d7231
Young Hahn <young@developmentseed.org>
2009-06-25 12:58:45 -0400

    Initial split of messaging/atrium shoutbox modules.

--- sites/all/modules/features/messaging_shoutbox/messaging_shoutbox.js
+++ sites/all/modules/features/messaging_shoutbox/messaging_shoutbox.js
@@ -1,19 +1,17 @@
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    if ($('div.shoutform').size() > 0) {
-      $("div.shoutform").parents('form').each(function() {
-        $(this).submit(function() {
-          var form = $(this);
-          $("input.form-submit", form).mousedown();
-          $("input.form-text", form).val('');
-          return false;
-        });
-        $("input.form-submit", this).mousedown(function() {
-          var form = $(this).parents('form');
-          $("input.form-text, textarea", form).val('');
-          return false;
-        });
+Drupal.behaviors.messaging_shoutbox = function (context) {
+  $('div.shoutform:not(.processed)').each(function() {
+    $(this).addClass('processed').parents('form').each(function() {
+      $(this).submit(function() {
+        var form = $(this);
+        $("input.form-submit", form).mousedown();
+        $("input.form-text", form).val('');
+        return false;
       });
-    }
+      $("input.form-submit", this).mousedown(function() {
+        var form = $(this).parents('form');
+        $("input.form-text, textarea", form).val('');
+        return false;
+      });
+    });
   });
 };
