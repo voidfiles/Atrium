ecd624259c88a641d5e2db966dfd53aa24b6ea1d
Young Hahn <yfhahn@gmail.com>
2009-09-06 22:27:15 -0400

    Updating scene to latest changes in Ginkgo.

--- sites/all/themes/scene/spaces-design.tpl.php
+++ sites/all/themes/scene/spaces-design.tpl.php
@@ -3,14 +3,21 @@
 body.spaces-design #features-menu li.active,
 body.spaces-design #features-menu li a:hover,
 body.spaces-design #global,
-body.spaces-design #header,
 body.spaces-design #branding,
-body.spaces-design #content h2.block-title,
-body.spaces-design #navigation ul.space-links li a:hover { background-color:<?php print $color ?>; }
+body.spaces-design #content h2.block-title { background-color:<?php print $color ?>; }
 
   body.spaces-design #features-menu li.active a:hover { background-color:transparent; }
 
 body.spaces-design #branding form input.form-text { background-color:<?php print $downshift ?>; }
+
+body.spaces-design #header div.block-widget,
+body.spaces-design #header div.toggle-blocks,
+body.spaces-design #header h2.block-title { border-color:<?php print $upshift ?>; }
+
+body.spaces-design #global,
+body.spaces-design #header h2.block-title,
+body.spaces-design #header div.block-widget a,
+body.spaces-design #global div.breadcrumb a { color:<?php print $upshiftplus ?>; }
 
 body.spaces-design #branding,
 body.spaces-design #navigation,
