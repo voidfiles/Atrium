9bddcb295bc3902909b6862dffa5568250883745
Young Hahn <yfhahn@gmail.com>
2009-09-14 11:31:36 -0400

    Updating to features 1.0-beta3.

--- sites/all/modules/contrib/features/features.js
+++ sites/all/modules/contrib/features/features.js
@@ -1,16 +1,62 @@
-// $Id: features.js,v 1.1.2.4 2009/06/07 15:43:57 yhahn Exp $
+// $Id: features.js,v 1.1.2.5 2009/09/13 23:23:23 yhahn Exp $
 
-Drupal.behaviors.featuresTable = function() {
-  $('table.features input').bind('change', function() {
-    if (!$(this).attr('checked')) {
-      $(this).parents('tr').removeClass('enabled').addClass('disabled');
-    }
-    else {
-      $(this).parents('tr').addClass('enabled').removeClass('disabled');
-    }
+Drupal.behaviors.features = function() {
+  // Features management form
+  $('table.features:not(.processed)').each(function() {
+    $(this).addClass('processed');
+
+    // Check the overridden status of each feature
+    Drupal.features.checkStatus();
+
+    // Add some nicer row hilighting when checkboxes change values
+    $('input', this).bind('change', function() {
+      if (!$(this).attr('checked')) {
+        $(this).parents('tr').removeClass('enabled').addClass('disabled');
+      }
+      else {
+        $(this).parents('tr').addClass('enabled').removeClass('disabled');
+      }
+    });
   });
 
-  Drupal.features.checkStatus();
+  // Export form component selector
+  $('form.features-export-form select.features-select-components:not(.processed)').each(function() {
+    $(this)
+      .addClass('processed')
+      .change(function() {
+        var target = $(this).val();
+        $('div.features-select').hide();
+        $('div.features-select-' + target).show();
+        return false;
+    });
+  });
+
+  // Export form machine-readable JS
+  $('.feature-name:not(.processed)').each(function() {
+    $('.feature-name')
+      .addClass('processed')
+      .after(' <small class="feature-project-suffix">&nbsp;</small>');
+    if ($('.feature-project').val() === $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-project').val() === '') {
+      $('.feature-project').parents('.form-item').hide();
+      $('.feature-name').keyup(function() {
+        var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
+        if (machine !== '_' && machine !== '') {
+          $('.feature-project').val(machine);
+          $('.feature-project-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
+            $('.feature-project').parents('.form-item').show();
+            $('.feature-project-suffix').hide();
+            $('.feature-name').unbind('keyup');
+            return false;
+          })).append(']');
+        }
+        else {
+          $('.feature-project').val(machine);
+          $('.feature-project-suffix').text('');
+        }
+      });
+      $('.feature-name').keyup();
+    }
+  });
 };
 
 Drupal.features = {
@@ -29,37 +75,11 @@ Drupal.features = {
             $(elem).find('.admin-default').show();
           }
           Drupal.features.checkStatus();
-        }, 'json')
+        }, 'json');
       }
       else {
           Drupal.features.checkStatus();
         }
     });
   }
-}
-
-Drupal.behaviors.featuresMachineReadable = function() {
-  if ($('.feature-name').size() > 0) {
-    $('.feature-name').after(' <small class="feature-module-name-suffix">&nbsp;</small>');
-    if ($('.feature-module-name').val() == $('.feature-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('.feature-module-name').val() == '') {
-      $('.feature-module-name').parents('.form-item').hide();
-      $('.feature-name').keyup(function() {
-        var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
-        if (machine != '_' && machine != '') {
-          $('.feature-module-name').val(machine);
-          $('.feature-module-name-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">'+ Drupal.t('Edit') +'</a>').click(function() {
-            $('.feature-module-name').parents('.form-item').show();
-            $('.feature-module-name-suffix').hide();
-            $('.feature-name').unbind('keyup');
-            return false;
-          })).append(']');
-        }
-        else {
-          $('.feature-module-name').val(machine);
-          $('.feature-module-name-suffix').text('');
-        }
-      });
-      $('.feature-name').keyup();
-    }
-  }
-}
+};
